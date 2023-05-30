c2b6a438ef87dc282cc88a0787b3c3052dddaa15
Josef <jdhamelink0@gmail.com>
2023-05-30 16:17:12 +0200

    Major cleanup
    Did break some stuff, but I'm gonna be refactoring datastructures anyway so

--- PN-main.py
+++ PN-main.py
@@ -1,50 +1,30 @@
-import networkx as nx
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
 import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 from matplotlib.widgets import Button 
 import random
-import time
+
 
 def main() -> None:
-    # Start of main program
-    tree = ET.parse('pathway.xml')
-    root = tree.getroot()
-    graph = nx.DiGraph()
-    global pathway
-    pathway = Pathway(
-        name=root.get('name'),
-        org=root.get('org'),
-        number=root.get('number'),
-        title=root.get('title'),
-        length=len(root)
-    )
+    """ Entry point. """
+    filename = 'pathway.xml'
+    global PW, AX, FI
+    PW = Pathway(filename)
 
-    # Initialize the pathway object. 
-    pathway = load_pathway(root, pathway, graph)
-
-
-
-    # Set the initial tokens for the genes
-    gene_tokens = {
-        'TLR1': 10,
-        'TLR3': 3,
-        'TLR4': 7,
-        'TLR5': 2
+    initial_marking = {
+        60: 10,  # TLR1
+        58: 3,   # TLR3
+        64: 7,   # TLR4
+        57: 2    # TLR5
     }
-    set_initial_tokens(pathway, gene_tokens)
 
-    # Create the node pairs for the visualization of all the nodes.
-    global node_pairs
-    node_pairs = create_node_pairs(pathway.nodes)
+    PW.set_initial_marking(initial_marking)
 
-    # for node1, node2 in node_pairs:
-    #     print(f'{node1} -> {node2}')
-
-    global ax
-    fig, ax = plt.subplots()
-    global frame_index
-    frame_index = 0
+    _, AX = plt.subplots()
+    FI = 0
 
     # Create the next frame button to display each frame of the pathway.
     next_frame_button_ax = plt.axes([0.8, 0.02, 0.1, 0.05])
@@ -58,7 +38,7 @@ def main() -> None:
 
 
     # Create the plot for the pathway.
-    update_plot(frame_index, pathway, node_pairs)
+    update_plot()
     plt.show()
 
     # animation = FuncAnimation(fig, update_plot, fargs=(node_pairs,), frames=100, interval=200, blit = False)
@@ -67,23 +47,97 @@ def main() -> None:
     # transition_pairs = get_transition_pairs(starting_nodes)    
 
 class Pathway:
-    def __init__(self, name, org, number, title, length):
-        self.name = name
-        self.org = org
-        self.number = number
-        self.title = title
-        self.length = length
-        self.nodes = []
-        self.transitions = []
-        self.groups = []
+    def __init__(self, filename: str) -> None:
+        """ Initialize the Pathway object from an KGML file. """
+        
+        assert os.path.exists(filename), \
+            f'File {filename} does not exist in folder {os.getcwd()}'
 
+        self.root = ET.parse(filename).getroot()
+        self.name = self.root.get('name'),
+        self.org = self.root.get('org'),
+        self.number = self.root.get('number'),
+        self.title = self.root.get('title'),
+        self.length = len(self.root)
+        self.nodes: list[Node] = []
+        self.transitions: list[Transition] = []
+        self.groups: list[Group] = []
 
-    def transfer_tokens(pathway):
+        self.__add_nodes()
+        self.__add_transitions()
+        self.__add_groups()
+        self.__create_node_pairs()
+        return
+
+    def __add_nodes(self) -> None:
+        """ Add all the nodes in the pathway. """
+        for entry in self.root.iter('entry'):
+            # TODO: other types of entries exist, but are not used for now
+            if entry.get('type') != 'gene':
+                continue
+            # initialize node object
+            node = Node(
+                node_type = entry.get('type'),
+                name = entry.get('name'),
+                node_id = int(entry.get('id')),
+            )
+            # parse graphics element
+            graphics = entry.find('graphics')
+            if graphics is None:
+                print(f'No graphics found for node {node.name}')
+                continue
+            graphics_name: str = graphics.get('name', '')
+            node.set_gene_name(graphics_name.split(', ')[0])
+            node.set_graphics(
+                x = float(graphics.get('x')) * 1.5,
+                y = float(graphics.get('y')) * 1.5,
+                width = float(graphics.get('width')) * 1.75,
+                height = float(graphics.get('height')) * 1.25,
+            )
+            # store node in list
+            self.nodes.append(node)
+        return
+
+    def __add_transitions(self) -> None:
+        """ Add all the transitions in the pathway. """
+        for transition in self.root.iter('relation'):
+            sender = int(transition.get('entry1'))
+            receiver = int(transition.get('entry2'))
+            # non-valid transitions link to a node with name "undefined"
+            # in this case, their IDs happen to be > 190
+            # TODO: make this work on other KGML files as well
+            if sender > 190 or receiver > 190:
+                continue
+            # very inefficient way to find the corresponding nodes
+            for node in self.nodes:
+                if node.id == sender:
+                    node.next_nodes.append(receiver)
+                if node.id == receiver:
+                    node.prev_nodes.append(sender)
+            # initialize transition object
+            transition_object = Transition(
+                entry1 = sender,
+                entry2 = receiver,
+                type = transition.get('subtype'),
+            )
+            # store transition in list
+            self.transitions.append(transition_object)
+        return
+    
+    def __add_groups(self) -> None:
+        """ Add all the groups in the pathway. """
+        for entry in self.root.iter('entry'):
+            if entry.get('type') == 'group':
+                group_object = Group(group_name=entry.get('name'))
+                self.groups.append(group_object)
+        return
+
+    def transfer_tokens(self) -> None:
         transfer_info = []
 
-        for node in pathway.nodes:
+        for node in self.nodes:
             # Check if the node has more than one next node and if it is not an inhibition transition
-            if len(node.next_nodes) > 1 and not any(transition.type == 'inhibition' for transition in pathway.transitions if transition.entry1 == node.id):
+            if len(node.next_nodes) > 1 and not any(transition.type == 'inhibition' for transition in self.transitions if transition.entry1 == node.id):
                 num_next_nodes = len(node.next_nodes)
                 transferred_tokens = node.tokens
 
@@ -94,22 +148,22 @@ class Pathway:
 
                     # Add the next nodes and the number of tokens to transfer to the transfer_info list.
                     for next_node_id in node.next_nodes[:-1]:
-                        next_node = next((n for n in pathway.nodes if n.id == next_node_id), None)
+                        next_node = next((n for n in self.nodes if n.id == next_node_id), None)
                         if next_node is not None:
                             transfer_info.append((node, next_node, tokens_to_transfer))
                     
                     # Add the last next node and the number of tokens to transfer to the transfer_info list.
                     last_next_node_id = node.next_nodes[-1]
-                    last_next_node = next((n for n in pathway.nodes if n.id == last_next_node_id), None)
+                    last_next_node = next((n for n in self.nodes if n.id == last_next_node_id), None)
                     if last_next_node is not None:
                         transfer_info.append((node, last_next_node, tokens_to_transfer + remaining_tokens))
            
             elif len(node.next_nodes) == 1:
                 next_node_id = node.next_nodes[0]
-                next_node = next((n for n in pathway.nodes if n.id == next_node_id), None)
+                next_node = next((n for n in self.nodes if n.id == next_node_id), None)
                 if next_node is not None:
                     relationship_type = None
-                    for transition in pathway.transitions:
+                    for transition in self.transitions:
                         if transition.entry1 == node.id and transition.entry2 == next_node.id:
                             relationship_type = transition.type
                             break
@@ -135,6 +189,30 @@ class Pathway:
                 # Print the token transfer information
                 print(f" ({transferred_tokens}) tokens {node.gene_name} ({node.id}) -> {next_node.gene_name} ({next_node.id}), {next_node.gene_name} tokens: {next_node.tokens}, next nodes: {next_node.next_nodes}")
 
+    def set_initial_marking(self, data: dict) -> None:
+        """ Adds tokens to some nodes in the pathway. """
+        for node in self.nodes:
+            if node.id in data:
+                node.add_token(data[node.id])
+        return
+
+    def __create_node_pairs(self) -> None:
+        """ Create pairs of nodes that are connected. """
+        pairs: dict = {}
+        for node in self.nodes:
+            current_id = node.id
+            for prev_id in node.prev_nodes:
+                # check if prev node exists in node list
+                if prev_id in [n.id for n in self.nodes]:
+                    pairs[(prev_id, current_id)] = True
+
+            for next_id in node.next_nodes:
+                # check if next node exists in node list
+                if next_id in [n.id for n in self.nodes]:
+                    # TODO: check if the group filter is necessary
+                    pairs[(current_id, next_id)] = True
+        self.node_pairs = pairs
+        return
 
 
 class Node:
@@ -172,7 +250,6 @@ class Node:
     def __str__(self):
         return f"Id: {self.id} Node: {self.name} (Tokens: {self.tokens}, Gene: {self.gene_name}, prev: {self.prev_nodes}, next: {self.next_nodes})"
 
-
 class Transition:
     def __init__(self, entry1, entry2, type):
         self.entry1 = entry1
@@ -187,125 +264,13 @@ class Group:
         self.name = group_name
 
 
-def add_nodes(root, pathway, graph):
-    # Iterate over the entry elements and create nodes
-    for entry in root.findall('entry[@type="gene"]'):
-        entry_id = int(entry.get('id'))
-        entry_name = entry.get('name')
-        entry_type = entry.get('type')
-
-        node = Node(node_type=entry_type, name=entry_name, node_id=entry_id)
-
-        # Get gene name from graphics section
-        graphics = entry.find('graphics')
-        if graphics is not None:
-            gene_name = graphics.get('name', '').split(',')[0].strip()
-            node.set_gene_name(gene_name)
-
-            nodeX =int(graphics.get('x')) * 1.5
-            nodeY =int(graphics.get('y')) * 1.5
-            nodeWidth =int( graphics.get('width')) * 1.75
-            nodeHeight = int(graphics.get('height')) * 1.2
-            node.set_graphics(nodeX, nodeY, nodeWidth, nodeHeight)
-
-        pathway.nodes.append(node)
-
-    return None
-
-def add_transitions(root, pathway, graph):
-    # Iterate over the transition elements and create transitions
-    for transition in root.findall('relation'):
-        sender = int(transition.get('entry1'))
-        receiver = int(transition.get('entry2'))
-
-        # if sender or receiver > 190 skip this transition
-        if sender > 190 or receiver > 190:
-            continue
-
-        # Add the receiver to the nextNodes of the sender.
-        for node in pathway.nodes:
-            if node.id == sender:
-                node.next_nodes.append(receiver)
-                # print(f"Added {receiver}  next_nodes of {sender}")
-            if node.id == receiver:
-                node.prev_nodes.append(sender)
-
-        subtype = transition.find('subtype')
-        subtype_name = subtype.get('name') if subtype is not None else None
-
-        transition_obj = Transition(entry1=sender, entry2=receiver, type=subtype_name)
-        pathway.transitions.append(transition_obj)
-
-        graph.add_edge(sender, receiver, type=subtype_name)
+def update_plot() -> None:
+    """ Updates the plot with the current token distribution. """
     
-    return None
+    AX.clear()
 
-
-
-
-def add_groups(root, pathway):
-    # Iterate over the group elements and create groups
-    for group in root.findall('entry[@type="group"]'):
-        group_name = group.get('name')
-
-        group_obj = Group(group_name=group_name)
-        pathway.groups.append(group_obj)
-    
-    return None
-    
-def load_pathway(root, pathway, graph):
-    add_nodes(root, pathway, graph)
-    add_transitions(root, pathway, graph)
-    add_groups(root, pathway)
-
-    return pathway
-
-def get_starting_nodes(pathway):
-    starting_nodes = {}
-
-    for node in pathway.nodes:
-        if node.gene_name is not None and node.gene_name.startswith('TLR'):    
-            starting_nodes[node.id] = node.next_nodes
-
-    return starting_nodes
-
-def create_node_pairs(nodes):
-    pairs = {}
-
-    for node in nodes:
-        current_id = node.id
-
-        for prev_id in node.prev_nodes:
-            if any(n.id == prev_id for n in nodes):  # Check if previous node exists in the list
-                pairs[(prev_id, current_id)] = True
-
-        for next_id in node.next_nodes:
-            next_node = next((n for n in nodes if n.id == next_id), None)  # Find the next node by ID
-            if next_node and next_node.type != 'group':  # Only consider non-group next nodes
-                pairs[(current_id, next_id)] = True
-
-    return pairs
-
-
-
-def get_transition_pairs(starting_nodes):
-    transition_pairs = []
-    for key, values in starting_nodes.items():
-        for value in values:
-            transition_pairs.append((key, value))
-    return transition_pairs
-
-def set_initial_tokens(pathway, gene_tokens):
-    for node in pathway.nodes:
-        if node.gene_name in gene_tokens:
-            node.add_token(gene_tokens[node.gene_name])
-
-
-def update_plot(frame, pathway, node_pairs):
-    ax.clear()
-
-    if frame != 0:
-        pathway.transfer_tokens()
+    if FI != 0:
+        PW.transfer_tokens()
 
     color_mappings = {
         'expression': 'blue',
@@ -315,7 +280,7 @@ def update_plot(frame, pathway, node_pairs):
         'inhibition': 'purple'
     }
 
-    for node in pathway.nodes:
+    for node in PW.nodes:
         if node.x is not None and node.y is not None and node.width is not None and node.height is not None:
             x = int(node.x)
             y = int(node.y)
@@ -323,66 +288,65 @@ def update_plot(frame, pathway, node_pairs):
             height = int(node.height)
 
             rect = plt.Rectangle((x, y), width, height, facecolor='lightblue', edgecolor='black')
-            ax.add_patch(rect)
-            ax.text(x + 0.4 * width, y + 0.5 * height, node.gene_name, ha='center', va='center', fontsize=6)
+            AX.add_patch(rect)
+            AX.text(x + 0.4 * width, y + 0.5 * height, node.gene_name, ha='center', va='center', fontsize=6)
             if node.tokens > 0:
-                ax.text(x + 0.7 * width, y + 5, f'{node.tokens}',  fontsize=7, color='red')
+                AX.text(x + 0.7 * width, y + 5, f'{node.tokens}',  fontsize=7, color='red')
             else: 
-                ax.text(x + 0.7 * width, y + 5, f'{node.tokens}',  fontsize=7, color='black')
+                AX.text(x + 0.7 * width, y + 5, f'{node.tokens}',  fontsize=7, color='black')
             
             # add the frame counter to the plot area
-            ax.text(0.5, 0.95, f'Frame: {frame}', transform=ax.transAxes, fontsize=10, verticalalignment='top', horizontalalignment='center')
+            AX.text(0.5, 0.95, f'Frame: {FI}', transform=AX.transAxes, fontsize=10, verticalalignment='top', horizontalalignment='center')
 
-            for pair in node_pairs:
-                if node.id == pair[0]:
+            for pair in PW.node_pairs.keys():
+                print(pair)
+                if pair[0] == node.id:
                     start_x = x + 0.5 * width
                     start_y = y + 0.5 * height
-                    for other_node in pathway.nodes:
+                    for other_node in PW.nodes:
                         if other_node.id == pair[1]:
                             end_x = int(other_node.x) + 0.5 * int(other_node.width)
                             end_y = int(other_node.y) + 0.5 * int(other_node.height)
                             relationship_type = None
-                            for transition in pathway.transitions:
+                            for transition in PW.transitions:
                                 if transition.entry1 == pair[0] and transition.entry2 == pair[1]:
                                     relationship_type = transition.type
                                     break
+                            print(relationship_type)
                             color = color_mappings.get(relationship_type, 'black')
-                            ax.plot([start_x + 0.5 * width, end_x - 0.5 * width], [start_y, end_y], color=color)
-                            ax.plot(start_x + 0.55 * width, start_y, 'go', markersize=5)
-                            ax.plot(end_x - 0.55 * width, end_y, 'ro', markersize=5)
+                            AX.plot([start_x + 0.5 * width, end_x - 0.5 * width], [start_y, end_y], color=color)
+                            AX.plot(start_x + 0.55 * width, start_y, 'go', markersize=5)
+                            AX.plot(end_x - 0.55 * width, end_y, 'ro', markersize=5)
 
     legend_elements = [
         plt.Line2D([0], [0], color=color, lw=1, label=relationship_type)
         for relationship_type, color in color_mappings.items()
     ]
-    ax.legend(handles=legend_elements, loc='upper left', fontsize=7)
+    AX.legend(handles=legend_elements, loc='upper left', fontsize=7)
 
-    ax.set_aspect('equal')
-    ax.set_xlim(0, 1800)
-    ax.set_ylim(0, 1200)
+    AX.set_aspect('equal')
+    AX.set_xlim(0, 1800)
+    AX.set_ylim(0, 1200)
 
-    ax.set_title('Petri Net Visualization')
+    AX.set_title('Petri Net Visualization')
     plt.draw()
+    return
 
-    return ax
-
-
-def next_frame(event):
-    global frame_index
-    frame_index += 1
-    if frame_index >= 100:
-        frame_index = 0
-    # animation._draw_next_frame(frame_index)
-    update_plot(frame_index, pathway, node_pairs)
+def next_frame(event: any) -> None:
+    global FI
+    FI += 1
+    if FI >= 100:
+        FI = 0
+    update_plot()
 
 
 def make_time_steps(event):
-    global frame_index
+    global FI
     for _ in range(5):
-        frame_index += 1
-        if frame_index >= 100:
-            frame_index = 0
-        update_plot(frame_index, pathway, node_pairs)
+        FI += 1
+        if FI >= 100:
+            FI = 0
+        update_plot()
         plt.pause(3)  # Pause for 5 seconds between each time-step
         plt.draw()
 
