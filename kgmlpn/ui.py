0fe42681b526965f1866e174f911582c2d764b08
Koen van der Burg <Koenvanderburg@live.nl>
2023-06-09 16:28:32 +0200

    Update ui.py

--- kgmlpn/ui.py
+++ kgmlpn/ui.py
@@ -6,7 +6,7 @@ from kgmlpn.pathway import Pathway
 
 # External imports
 import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle, Arrow
+from matplotlib.patches import Rectangle, FancyArrow
 
 
 def update_plot(ax: plt.Axes, pw: Pathway, G : bool = False) -> None:
@@ -29,21 +29,40 @@ def update_plot(ax: plt.Axes, pw: Pathway, G : bool = False) -> None:
     # Draw nodes.
     for node in pw.nodes.values():
         x, y, w, h = node.graph_props.values()
-
+        
+        # Add knockout nodes.
         if node.knockout:
             edgecolor = 'red'
-            ax.add_patch(Rectangle((x, y), w, h, facecolor='lightgrey', edgecolor=edgecolor))
-            # Add diagonal lines
+            ax.add_patch(Rectangle((x, y), w, h,
+                         facecolor='lightgrey',
+                         edgecolor=edgecolor))
+            # Add knockout symbol.
             ax.plot([x, x+w], [y, y+h], color=edgecolor)
             ax.plot([x+w, x], [y, y+h], color=edgecolor)
 
+        # Add normal nodes.
         if not node.knockout: 
             edgecolor = 'hotpink' if node.tokens else 'black'
             linestyle = (0, (5,1)) if node.tokens else 'solid'
-            ax.add_patch(Rectangle((x, y), w, h, facecolor='lightblue', edgecolor=edgecolor, linewidth = 1.5, linestyle = linestyle))
-            ax.text(x + 0.4 * w, y + 0.5 * h, node.name, ha='center', va='center', fontsize=6)
+
+            ax.add_patch(Rectangle((x, y), w, h,
+                            facecolor='lightblue',
+                            edgecolor=edgecolor,
+                            linewidth = 1.5,
+                            linestyle = linestyle))
+
+            ax.text(x + 0.4 * w, y + 0.5 * h,
+                    node.name,
+                    ha='center',
+                    va='center',
+                    fontsize=6)
+
+            # Add token count.
             if node.tokens:
-                ax.text(x + 0.7 * w, y + 5, f'{node.tokens}',  fontsize=7, color='black')
+                ax.text(x + 0.7 * w, y + 5,
+                        f'{node.tokens}',
+                        fontsize=7,
+                        color='black')
 
     # Draw grouped nodes. 
     if G:
@@ -62,17 +81,27 @@ def update_plot(ax: plt.Axes, pw: Pathway, G : bool = False) -> None:
         from_y += from_h / 2
         to_y += to_h / 2
         color = color_mappings[transition.name]
-        ax.add_patch(Arrow(from_x, from_y, to_x - from_x, to_y - from_y, width=0.5, color=color))
+        ax.add_patch(FancyArrow(
+            from_x, from_y, to_x - from_x, to_y - from_y,
+            width=0.1,
+            color=color,
+            head_width= 2,
+            overhang= 0.9,
+            length_includes_head=True))
 
     legend_elements = [
-        Arrow(0, 0, 0, 0, width=0.5, color=color, label=relationship_type)
+        FancyArrow(0, 0, 0, 0, 
+                width=0.5,
+                color=color,
+                label=relationship_type)
         for relationship_type, color in color_mappings.items()
     ]
     ax.legend(handles=legend_elements, loc='upper left', fontsize=7)
 
     ax.set_aspect('equal')
     ax.set_xlim(0, 1800)
-    ax.set_ylim(0, 1200)
+    ax.set_ylim(100, 1200)
+    ax.axis('off')
     ax.set_title('Petri Net Visualization')
 
     return
