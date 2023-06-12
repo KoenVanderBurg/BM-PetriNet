cf64a5142451a9ccc1cd1cdf3acf32e1680047c6
Koen van der Burg <Koenvanderburg@live.nl>
2023-06-12 18:43:14 +0200

    githuc action flex push

--- tests/test.py
+++ tests/test.py
@@ -15,19 +15,12 @@ def pathway():
 def test_pathway_nodes(pathway):
     # for node in pathway.nodes.values():
         # print(node)
-    # print(len(pathway.nodes))
     assert len(pathway.nodes) > 0
 
 def test_pathway_groups(pathway):
-    # for group in pathway.groups.values():
-    #     print(group)
-    # print(len(pathway.groups))
 
     assert len(pathway.groups) > 0
 
 def test_pathway_transitions(pathway):
-    # for transition in pathway.transitions:
-    #     print(transition)
-    # print(len(pathway.transitions))
-    
+
     assert len(pathway.transitions) > 0
