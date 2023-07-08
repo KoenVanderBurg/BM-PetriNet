9a554737e94c6e524782eb8328cd294d2765a832
Koen van der Burg <Koenvanderburg@live.nl>
2023-07-08 20:43:40 +0200

    changes to support pyqt6

--- src/KGML_PN/main.py
+++ src/KGML_PN/main.py
@@ -1,15 +1,17 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QGridLayout, QLabel, QLineEdit
-from PyQt5.QtGui import QFont
-from PyQt5.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QGridLayout
+from PyQt6.QtGui import QFont
+from PyQt6 import QtCore
+
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from KGML_PN.pathway import Pathway
 from KGML_PN.ui import update_plot
 
+
 class NetworkPlotWidget(QWidget):
     def __init__(self):
-        super(NetworkPlotWidget, self).__init__()
+        super().__init__()
         self.figure = Figure()
         self.canvas = FigureCanvas(self.figure)
         self.ax = self.figure.add_subplot(111)
@@ -31,36 +33,45 @@ class NetworkPlotWidget(QWidget):
                 node_info = self.ax.format_coord(x, y)
                 print("Selected Node:", node_info)
 
+                # TODO: instead of printing "selected node" use location to find gene.
+
+
 class MainWindow(QMainWindow):
+    """This class contains the code to create main UI in which the network and interactive buttons are defined. """
+
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super().__init__()
 
         pathway = "pathway.xml"
 
+        # Create the main widget.
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
 
+        # Define the layout size.
         layout = QHBoxLayout(central_widget)
 
         buttons_widget = QWidget()
         buttons_layout = QGridLayout(buttons_widget)
 
+        # Define various buttons in the UI.
         frame_button = QPushButton("Next Frame")
         show_g_button = QPushButton("Show Groups")
         rm_g_button = QPushButton("Remove Groups")
         knockout_button = QPushButton("Set Knockouts")
         buttons = [frame_button, show_g_button, rm_g_button, knockout_button]
 
-        button_font = QFont("Times", 10, QFont.Bold)
+        # Set the button
+        button_font = QFont("Times New Roman", 10)
         for i, button in enumerate(buttons):
             button.setFont(button_font)
             button.setFixedSize(175, 100)
             buttons_layout.addWidget(button, i // 2, i % 2)
             button.setStyleSheet("background-color: #add8e6; border-radius: 10px; border: 2px solid #000000;")
 
-    
         buttons_layout.setSpacing(20)
-        buttons_layout.setAlignment(Qt.AlignBottom | Qt.AlignLeft)
+        buttons_layout.setRowStretch(2, 1)
+        buttons_layout.setColumnStretch(1, 1)
 
         plotting_widget = NetworkPlotWidget()
 
@@ -105,8 +116,9 @@ class MainWindow(QMainWindow):
         self.pw.set_knockouts(knockouts)
         self.plotting_widget.update_plot(self.pw, self.G)
 
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
--- src/KGML_PN/ui.py
+++ src/KGML_PN/ui.py
@@ -8,8 +8,12 @@ from KGML_PN.pathway import Pathway
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle, FancyArrow
 
-def update_plot(ax: plt.Axes, pw: Pathway, G : bool = False) -> None:
-    """ Updates the plot with the current token distribution. """
+def update_plot(ax: plt.Axes, pw: Pathway, set_groups : bool = False) -> None:
+    """ 
+    Updates the plot with the current token distribution.
+    set_groups is used to view the different groupings of the network.   
+    """
+
     ax.clear()
     color_mappings = {
         'expression': 'blue',
@@ -22,11 +26,11 @@ def update_plot(ax: plt.Axes, pw: Pathway, G : bool = False) -> None:
         'compound': 'yellow',
         'undefined': 'black',
     }
-    # Draw nodes.
+    # Draw the nodes found in the network. 
     for node in pw.nodes.values():
         x, y, w, h = node.graph_props.values()
         
-        # Add knockout nodes.
+        # Upon being set as knockout node, apply different visual.
         if node.knockout:
             edgecolor = 'red'
             ax.add_patch(Rectangle((x, y), w, h,
@@ -35,30 +39,35 @@ def update_plot(ax: plt.Axes, pw: Pathway, G : bool = False) -> None:
             # Add knockout symbol.
             ax.plot([x, x+w], [y, y+h], color=edgecolor)
             ax.plot([x+w, x], [y, y+h], color=edgecolor)
-        # Add normal nodes.
+
+        # Add normal (non-knockout) nodes.
         if not node.knockout: 
-            edgecolor = 'hotpink' if node.tokens else 'black'
-            linestyle = (0, (5,1)) if node.tokens else 'solid'
+            edgecolor = 'hotpink' if node.tokens else 'black' # sets node edgecolor based on token amount. 
+            linestyle = (0, (5,1)) if node.tokens else 'solid' # sets node edge linestyle based on token availiability. 
             ax.add_patch(Rectangle((x, y), w, h,
                             facecolor='lightblue',
                             edgecolor=edgecolor,
                             linewidth = 1.5,
                             linestyle = linestyle))
+            
+            # Sets the gene name in the node. 
             ax.text(x + 0.4 * w, y + 0.5 * h,
                     node.name,
                     ha='center',
                     va='center',
                     fontsize=6)
-            # Add token count.
+            
+            # Add token count to the node.
             if node.tokens:
                 ax.text(x + 0.7 * w, y + 5,
                         f'{node.tokens}',
                         fontsize=7,
                         color='black')
-    # Draw groups around nodes. . 
-    if G:
-        for node in pw.groups.values():
-            x, y, w, h = node.graphics.values()
+                
+    # Draw groups around nodes.
+    if set_groups:
+        for group in pw.groups.values():
+            x, y, w, h = group.graphics.values()
             ax.add_patch(Rectangle(
                         (x - (0.1 * w), y - (0.45 * h)),
                         w * 1.25, 
@@ -71,18 +80,26 @@ def update_plot(ax: plt.Axes, pw: Pathway, G : bool = False) -> None:
                         
             ax.text(x + 0.4 * w, 
                     y + 1.1 * h, 
-                    node.id,
+                    group.id,
                     ha='center',
                     va='center',
                     fontsize=7)
+   
+    # Plot the transitions between nodes.
     for transition in pw.transitions:
+
+        # Get node objects involved with transition.
         from_node = pw.nodes[transition.from_id]
         to_node = pw.nodes[transition.to_id]
+        
+        # Retrieve graphical information from both nodes, and adjust for node width. 
         from_x, from_y, from_w, from_h = from_node.graph_props.values()
         to_x, to_y, to_w, to_h = to_node.graph_props.values()
         from_x += from_w
         from_y += from_h / 2
         to_y += to_h / 2
+
+        # Add the color corresponding to the transition name. 
         color = color_mappings[transition.name]
         ax.add_patch(FancyArrow(
             from_x, from_y, to_x - from_x, to_y - from_y,
@@ -91,6 +108,8 @@ def update_plot(ax: plt.Axes, pw: Pathway, G : bool = False) -> None:
             head_width= 2,
             overhang= 0.9,
             length_includes_head=True))
+
+    # Add the different transition types legenda.  
     legend_elements = [
         FancyArrow(0, 0, 0, 0, 
                 width=0.5,
@@ -98,6 +117,8 @@ def update_plot(ax: plt.Axes, pw: Pathway, G : bool = False) -> None:
                 label=relationship_type)
         for relationship_type, color in color_mappings.items()
     ]
+
+    # Plotting the network inside the UI. 
     ax.legend(handles=legend_elements, loc='upper left', fontsize=7)
     ax.set_aspect('equal')
     ax.set_xlim(0, 1800)
